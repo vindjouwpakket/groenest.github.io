4657adcfc17a88cdf048760fa64ca25e1064aed5
Lucas Vahle <56959974+Lucas42313456@users.noreply.github.com>
2021-01-29 13:41:51 +0100

    transfer
    transfer from wwwwebsite to github.io

---
+++ scripts/demo.js
@@ -0,0 +1,10 @@
+
+function display() {
+alert('hello world');
+}
+var x = 100;
+document.write(x);
+
+document.write('<h1>A</h1>');
+/*This a comment line */
+
---
+++ scripts/main.js
@@ -0,0 +1,37 @@
+const myHeading = document.querySelector('h1');
+myHeading.textContent= 'Hello world!';
+
+var iceCream = 'chocolate';
+if (iceCream === 'chocolate'){
+    alert('Yay, I love chocolate icecream');
+} else {
+    alert('Awwww, but choclate is my favourite...');
+}
+
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images\firefox-icon.png') {
+      myImage.setAttribute('src','images\tree.png');
+    } else {
+      myImage.setAttribute('src','images\firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+  }
+    if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+myButton.onclick = function() {
+setUserName();
+  }
